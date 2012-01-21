3f5a04c6ea03d1f538450db15bb3ef776d398572
Alan Baird <abaird@bairdsnet.net>
2012-01-21 16:00:26 -0600

    w00t! - passing specs

--- lib/word_cloud/generator.rb
+++ lib/word_cloud/generator.rb
@@ -1,17 +1,54 @@
 class WordCloudinator
-  def initialize(file="data.csv")
-    @words = {}
-    @browser = WordleBrowser.new
+  attr_reader :color
+
+  def initialize(file="data.csv", color=:random)
+    @words = words_to_hash(FasterCSV.read(File.join(File.dirname(__FILE__), file)))
+    @color = color
+  end
+
+  def words_to_hash(data)
+    words = {}
+    data.each do |row|
+      words[row[0]] = row[1].to_i
+    end
+    words
   end
 
   def generate_text
-    
+    str = ''
+    words = @words.keys.sort
+    words.each { |word| str << "#{word}:#{@words[word]}\n" }
+    str
   end
 end
 
 class WordleBrowser
-  def initialize
+  attr_reader :browser
+  def initialize(wc)
     @browser = Watir::Browser.new
-    @browser.goto('http://wordle.net')
+    @browser.goto('http://www.wordle.net/advanced')
+    @wc = wc
+    @specify_color = wc.color == :random ? false : true
+  end
+
+  def words_field; @browser.text_field(:id, 'wordcounts'); end
+  def color_words_field; @browser.text_field(:id, 'colorwordcounts'); end
+  def background_color; @browser.text_field(:id, 'bg'); end
+
+  def dump_to_wordle
+    if @specify_color
+      color_words_field.value = @wc.generate_text
+    else
+      words_field.value = @wc.generate_text
+    end
+    submit
+  end
+
+  def submit
+    if @specify_color
+      @browser.button(:class => 'submit', :index => 1).click
+    else
+      @browser.div(:id, 'controlpanel').element(:class=>'submit').click
+    end
   end
 end
--- spec/word_cloud_spec.rb
+++ spec/word_cloud_spec.rb
@@ -3,18 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "WordCloud" do
   before(:all) do
     @wc = WordCloudinator.new("test_data.csv")
-    good_result = <<-TEXT
+    @good_result = <<-TEXT
 bananas:3
-pears:2
-orange:1
+oranges:2
+pears:1
     TEXT
   end
   it "should generate 3 bananas, 2 pears and 1 orange" do
-    @wc.generate_text.should == good_result
+    @wc.generate_text.should == @good_result
   end
 
   it "should put the generated text in the wordle site" do
-    @wc.dump_to_wordle
-    @wc.browser.wordle_text.should == good_result
+    wb = WordleBrowser.new(@wc)
+    wb.dump_to_wordle
+    wb.browser.url.should == "http://www.wordle.net/compose"
   end
 end
